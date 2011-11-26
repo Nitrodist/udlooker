100a566b4a363eb812269f9638ab872997305364
Mark Campbell <nitrodist@gmail.com>
2011-11-26 15:47:00 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Udlooker::Application.config.secret_token = 'bed71127a9d67226d133c1e93cda5d712366d4633207d39733c08ffaca30ef564187728b4506abe35e2dfc4b3bd9522c2ed7a3562906e4f14f8e2a4cbd0ed52e'
